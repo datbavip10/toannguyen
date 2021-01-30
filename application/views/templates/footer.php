99eea091f0e1f8383aae06a388ad04d680ba59dc
Dat.Do <datbavip.computer@gmail.com>
2021-01-30 14:17:01 +0700

    First Commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,16 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Phiên bản</b> 0.1.0
+    </div>
+    <strong>Bản quyền &copy; ABC Inc. 2018-<?php echo date('Y') ?>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
